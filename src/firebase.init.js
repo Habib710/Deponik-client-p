f18a8c27fc954a40dc1040358e238bbb1175e057
habib <mdhabib71024@gmail.com>
2022-04-29 00:20:14 +0600

    basic setup

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth" ;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKwpUN7KmDlS21FZhEqxnF81rgRebZsSw",
+  authDomain: "deponic-62e80.firebaseapp.com",
+  projectId: "deponic-62e80",
+  storageBucket: "deponic-62e80.appspot.com",
+  messagingSenderId: "856412438991",
+  appId: "1:856412438991:web:b1c7e3108be40fddc1d2ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app);
+
+ export default auth ;
