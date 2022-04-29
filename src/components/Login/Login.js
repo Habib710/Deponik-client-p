8d90328dd2c005565fd3d0f142905bdb4374f14c
habib <mdhabib71024@gmail.com>
2022-04-29 17:42:45 +0600

    authantication in prosses

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import { faEnvelope, faLock,   } from '@fortawesome/free-solid-svg-icons';
 import './Login.css';
 
 const Login = () => {
+    const [email,setemail]=useState([]);
+    const [password,setpassword]=useState([]);
+    const navigate=useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    const emailhendle=event=>{
+        setemail(event.target.value);
+        
+
+    }
+    const passwordhendle=event=>{
+       setpassword(event.target.value);
+       
+    }
+    const login=event=>{
+        event.preventDefault()
+        signInWithEmailAndPassword(email, password)
+    }
+    if(user){
+        
+        navigate('/blog')
+    }
     return (
         <div className='login-main-div py-5 px-2'>
             <div className=' container  form-custom-css'>
             
-            <Form className='form-input-css'>
+            <Form onSubmit={login} className='form-input-css'>
             <h3>Login</h3>
                 <div className='input-css'>
 
 
                 <span className='d-flex align-items-center  justify-content '><FontAwesomeIcon className='mt-4 me-2' icon={faEnvelope} /> 
-                 <input type="email" name='name' placeholder=' Your Email'/></span>
+                 <input onBlur={emailhendle} type="email"  placeholder=' Your Email'/></span>
 
 
                  <span className='d-flex align-items-center  justify-content '><FontAwesomeIcon className='mt-4 me-2' icon={faLock} /> 
-               <input type="password" name='password'  placeholder=' Your Password'/></span>
+               <input onBlur={passwordhendle} type="password"   placeholder=' Your Password' required/></span>
 
 
                </div>
--- src/components/singUp/SingUp.js
+++ src/components/singUp/SingUp.js
@@ -1,29 +1,70 @@
 import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './singup.css';
 
 const SingUp = () => {
+    const [email,setemail]=useState([]);
+    const [password,setpassword]=useState([]);
+    const [conpass,setconpass]=useState([]);
+    const navigate=useNavigate();
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
+    const NameHendle =event=>{
+        console.log(event.target.value);
+    }
+    const emailhendle=event=>{
+        setemail(event.target.value);
+        console.log(event.target.value);
+
+    }
+    const passwordhendle=event=>{
+       setpassword(event.target.value);
+       console.log(event.target.value);
+    }
+    const conpasshendle=event=>{
+        setconpass(event.target.value);
+    }
+    const onsubmit=event=>{
+        event.preventDefault()
+        createUserWithEmailAndPassword(email, password)
+
+
+    }
+    if(user){
+        
+        navigate('/blog')
+    }
     return (
         <div className='py-5 px-2 d-lg-flex  align-items-center  singup-custom-div'>
         
             <div className=' container singup-custom  form-custom-css '>
             
-            <Form className='form-input-css'>
+            <Form onSubmit={onsubmit} className='form-input-css'>
             <h3>Sing Up</h3>
                 <div className='input-css'>
                 <span className='d-flex align-items-center   '><FontAwesomeIcon className='mt-4 me-2' icon={faUser} /> 
-                 <input type="text" name='name' placeholder=' Your Name'/></span>
+                 <input onBlur={NameHendle} type="text" placeholder=' Your Name'/></span>
+
                 <span className='d-flex align-items-center  '><FontAwesomeIcon className='mt-4 me-2' icon={faEnvelope} /> 
-                 <input type="email" name='email' placeholder=' Your Email'/></span>
+                 <input onBlur={emailhendle} type="email"  placeholder=' Your Email'/></span>
 
                  <span className='d-flex align-items-center  nt '><FontAwesomeIcon className='mt-4 me-2' icon={faLock} /> 
-               <input type="password" name='password'  placeholder=' Your Password'/></span>
+               <input onBlur={passwordhendle} type="password"   placeholder=' Your Password'/></span>
 
                  <span className='d-flex align-items-center   '><FontAwesomeIcon className='mt-4 me-2' icon={faLock} /> 
-               <input type="password" name='conpassword' placeholder='Confrim Your Password'/></span>
+               <input onBlur={conpasshendle} type="conpassword"  placeholder='Confrim Your Password'/></span>
                </div>
                <br /><br />
                <input className='submit-css w-50' type="submit" value='Sing Up' />
