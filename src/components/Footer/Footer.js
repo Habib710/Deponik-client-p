4cf564153423f944bc4aaa799244ad2b4618b89b
habib <mdhabib71024@gmail.com>
2022-05-01 13:28:53 +0600

    extra section add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className='footer-css'>
+        <div className='footer-css py-5'>
             <div className='text-div text-white text-center container'>
                 <h2>D E P O <span className='deponic'>N I C</span></h2>
                 <h5>All The Right Reserved</h5>
